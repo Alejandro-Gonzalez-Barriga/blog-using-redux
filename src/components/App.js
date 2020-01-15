788df80f02fd81368d5456f33cd5cd031e8e596d
alexG <alejandro.gonzalez.barriga@gmail.com>
2020-01-14 19:03:02 -0600

    re-structure the app and usuarios component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,48 @@
-import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import Usuarios from "./Usuarios/index";
+import React, { Component } from "react";
+import axios from "axios";
 
-import Menu from "./Menu";
+class Usuarios extends Component {
+  constructor() {
+    super();
+    this.state = {
+      usuarios: []
+    };
+  }
 
-const App = () => (
-  <BrowserRouter>
-    <Menu />
-    <Usuarios />
-    <Route exact path="/"></Route>
-  </BrowserRouter>
-);
+  async componentDidMount() {
+    const respuesta = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    this.setState({
+      usuarios: respuesta.data
+    });
+  }
 
-export default App;
+  ponerFilas = () =>
+    this.state.usuarios.map(usuario => (
+      <tr key={usuario.id}>
+        <td>{usuario.name}</td>
+        <td>{usuario.email}</td>
+        <td>{usuario.website}</td>
+      </tr>
+    ));
+
+  render() {
+    return (
+      <div>
+        <table className="tabla">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Correo</th>
+              <th>Enlace</th>
+            </tr>
+          </thead>
+          <tbody>{this.ponerFilas()}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Usuarios;
