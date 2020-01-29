183cfc0bbe47c3786fe6b23bc2ec0ee06c438368
alexG <alejandro.gonzalez.barriga@gmail.com>
2020-01-29 10:24:42 -0600

    refactor code to get usuarios' publications and store them in cache

--- src/actions/publicacionesActions.js
+++ src/actions/publicacionesActions.js
@@ -1,38 +1,20 @@
 import axios from "axios";
-import { TRAER_TODOS, LOADING, ERROR } from "../types/publicacionesTypes";
+import { TRAER_POR_USUARIO, LOADING, ERROR } from "../types/publicacionesTypes";
 import usuariosReducer from "../reducers/usuariosReducer";
-
-export const traerTodos = () => async dispatch => {
-  dispatch({
-    type: LOADING
-  });
-
-  try {
-    const respuesta = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts"
-    );
-    dispatch({
-      type: TRAER_TODOS,
-      payload: respuesta.data
-    });
-  } catch (error) {
-    console.log("Error:", error.message);
-    dispatch({
-      type: ERROR,
-      payload: "Upss, something went wrong"
-    });
-  }
-};
 
 export const traerPorUsuario = key => async (dispatch, getState) => {
   const { usuarios } = getState().usuariosReducer;
+  const { publicaciones } = getState().publicacionesReducer;
   const usuario_id = usuarios[key].id;
 
   const respuesta = await axios.get(
     `https://jsonplaceholder.typicode.com/posts?userId=${usuario_id}`
   );
+
+  const publicaciones_actualizadas = [...publicaciones, respuesta.data];
+
   dispatch({
-    type: TRAER_TODOS,
-    payload: respuesta.data
+    type: TRAER_POR_USUARIO,
+    payload: publicaciones_actualizadas
   });
 };
