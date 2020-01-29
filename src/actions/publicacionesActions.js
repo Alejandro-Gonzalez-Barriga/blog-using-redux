acc35f2008269e7a540de338fb9115eeb547327f
alexG <alejandro.gonzalez.barriga@gmail.com>
2020-01-29 16:10:44 -0600

    handeled user publications inmitability

--- src/actions/publicacionesActions.js
+++ src/actions/publicacionesActions.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { TRAER_POR_USUARIO, LOADING, ERROR } from "../types/publicacionesTypes";
-import usuariosReducer from "../reducers/usuariosReducer";
+import * as usuariosTypes from "../types/usuariosTypes";
+
+const { TRAER_TODOS: USUARIOS_TRAER_TODOS } = usuariosTypes;
 
 export const traerPorUsuario = key => async (dispatch, getState) => {
   const { usuarios } = getState().usuariosReducer;
@@ -12,6 +14,20 @@ export const traerPorUsuario = key => async (dispatch, getState) => {
   );
 
   const publicaciones_actualizadas = [...publicaciones, respuesta.data];
+
+  const publicaciones_key = publicaciones_actualizadas.length - 1;
+
+  const usuarios_actualizados = [...usuarios];
+
+  usuarios_actualizados[key] = {
+    ...usuarios[key],
+    publicaciones_key
+  };
+
+  dispatch({
+    type: USUARIOS_TRAER_TODOS,
+    payload: usuarios_actualizados
+  });
 
   dispatch({
     type: TRAER_POR_USUARIO,
