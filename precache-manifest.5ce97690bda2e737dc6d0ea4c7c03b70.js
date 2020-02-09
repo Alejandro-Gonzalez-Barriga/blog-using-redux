3ec9be678bc8447dcc131d186d14f9191c9e515c
alexG <alejandro.gonzalez.barriga@gmail.com>
2020-02-09 17:33:33 -0600

    Updates

---
+++ precache-manifest.5ce97690bda2e737dc6d0ea4c7c03b70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3486eecd5f589f5823f1370c1d10ca9",
+    "url": "/blog-using-redux/index.html"
+  },
+  {
+    "revision": "e1923dd4e9797f3979c8",
+    "url": "/blog-using-redux/static/css/main.f5e164c4.chunk.css"
+  },
+  {
+    "revision": "2f7c28554ad108a87aa3",
+    "url": "/blog-using-redux/static/js/2.64b127b8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/blog-using-redux/static/js/2.64b127b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1923dd4e9797f3979c8",
+    "url": "/blog-using-redux/static/js/main.6b01141d.chunk.js"
+  },
+  {
+    "revision": "bc002a0fb0779dd8606c",
+    "url": "/blog-using-redux/static/js/runtime-main.e965083a.js"
+  }
+]);
--- src/components/general/Fatal.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Fatal = props => <h2 className="center danger">{props.messege}</h2>;
-
-export default Fatal;
