9423cc8ad789e69cac7519b74e62adda7d9c4d65
alexG <alejandro.gonzalez.barriga@gmail.com>
2020-02-12 17:23:24 -0600

    normalized todo response

--- src/actions/tareasActions.js
+++ src/actions/tareasActions.js
@@ -6,12 +6,25 @@ export const traerTodas = () => async dispatch => {
     const respuesta = await axios.get(
       "https://jsonplaceholder.typicode.com/todos"
     );
+
+    const tareas = {};
+    respuesta.data.map(
+      tar =>
+        (tareas[tar.userId] = {
+          ...tareas[tar.userId],
+          [tar.id]: {
+            ...tar
+          }
+        })
+    );
+    console.log(tareas);
+
     dispatch({
       type: LOADING
     });
     dispatch({
       type: TRAER_TODAS,
-      payload: respuesta.data
+      payload: tareas
     });
   } catch (error) {
     console.log("Error:", error.message);
